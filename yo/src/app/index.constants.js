84bfdd23577dcd803297418f782b769b584c532d
kelto <charles.fallourd@gmail.com>
2015-09-26 16:51:17 +0200

    create angular app with yo. Close #9

---
+++ yo/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('yo')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
