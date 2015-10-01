29ca81f037e13be408aea7a2fcdf600783d2a853
kelto <charles.fallourd@gmail.com>
2015-10-01 09:55:02 +0200

    remove useless script

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,37 +1,6 @@
-import astrovisor.Planet
-
 class BootStrap {
 
     def init = { servletContext ->
-        environments {
-            production {
-                "cd yo && gulp".execute();
-            }
-
-            development {
-                "cd yo && gulp serve".execute();
-            }
-        }
-        def result = '################## running in UNCLEAR mode.'
-        println "Application starting ... "
-        seedTestData()
-
-        println "$result"
-    }
-
-    private void seedTestData() {
-        def planet = null
-        println "Start loading cities into database"
-        planet = new Planet(name: 'kelto')
-        assert planet.save(failOnError:true, flush:true, insert: true)
-        planet.errors = null
-
-        planet = new Planet(name: 'keltorin')
-        assert planet.save(failOnError:true, flush:true, insert: true)
-        planet.errors = null
-
-        assert Planet.count == 2;
-        println "Finished loading $Planet.count cities into database"
     }
         def destroy = {
     }
