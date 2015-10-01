c35e8c8a0ef6c7d32d142260a86fc9c159a1e8b9
kelto <charles.fallourd@gmail.com>
2015-10-01 10:45:42 +0200

    create test data for planets

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,24 @@
+import astrovisor.Planet
+
 class BootStrap {
 
     def init = { servletContext ->
+        seedTestData()
+    }
+
+    private void seedTestData() {
+        def planet = null
+        println "Start loading planets into database"
+        planet = new Planet(code_name: "XO-000",age:0, name: 'kelto')
+        assert planet.save(failOnError:true, flush:true, insert: true)
+        planet.errors = null
+
+        planet = new Planet(code_name: "XO-001",age:0, name: 'keltorin')
+        assert planet.save(failOnError:true, flush:true, insert: true)
+        planet.errors = null
+
+        assert Planet.count == 2;
+        println "Finished loading $Planet.count planets into database"
     }
         def destroy = {
     }
