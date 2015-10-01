f91d5aeed87d8c9465a2d1b19179b54d149fe948
kelto <charles.fallourd@gmail.com>
2015-10-01 09:55:50 +0200

    add new attribut to planet and create test

--- grails-app/domain/astrovisor/Planet.groovy
+++ grails-app/domain/astrovisor/Planet.groovy
@@ -4,8 +4,14 @@ import grails.rest.*
 @Resource(uri='/planet')
 class Planet {
 
+    String code_name
     String name
+    int age
+
 
     static constraints = {
+        code_name blank: false, unique: true, nullable: false
+        name blank: false, nullable: true
+        age min: 0
     }
 }
--- test/unit/astrovisor/PlanetSpec.groovy
+++ test/unit/astrovisor/PlanetSpec.groovy
@@ -1,9 +1,7 @@
 package astrovisor
-
 import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
 import spock.lang.Specification
-
 /**
  * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
@@ -16,6 +14,42 @@ class PlanetSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    //todo : change last row planet_age to 100, should still pass.
+    void "test invalid planet"() {
+        given : "An invalid planet"
+        Planet planet = new Planet(code_name: planet_code_name, age: planet_age, name: planet_name)
+
+        when: "We try to validate"
+        def isValid = planet.validate()
+
+        then: "The validation fail"
+        isValid == false
+
+        where:
+        planet_code_name | planet_age | planet_name
+        ""               | 0          | null
+        "XO-344"         | -1         | null
+        null             | 100        | null
+        "XO-355"         | -100        | ""
+
+    }
+
+    void "test valid planet" () {
+        given: "A valid planet"
+        Planet planet = new Planet(code_name: planet_code_name, age: planet_age, name: planet_name)
+        println planet.name
+
+        when: "We try to validate"
+        def isValid = planet.validate()
+
+        then: "The validation fail"
+        isValid == true
+
+        where:
+        planet_code_name | planet_age | planet_name
+        "Test"           | 0          | null
+        "XO-345"         | 1          | "HelloPlanet"
+        "XA-033"         | 100        | null
+        "XO-356"         | 100        | "ByePlanet"
     }
 }
