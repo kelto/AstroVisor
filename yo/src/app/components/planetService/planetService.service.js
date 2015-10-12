b83d6f0dd4288f7e7aa2e5ee168bb938797b74c3
wallart <julien.wallart@phelsuma.fr>
2015-10-12 21:46:52 +0200

    Enhancement planetService

--- yo/src/app/components/planetService/planetService.service.js
+++ yo/src/app/components/planetService/planetService.service.js
@@ -1,28 +1,43 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular.module('yo').factory('planetService', planetService);
+  angular.module('yo').factory('planetService', planetService);
 
-    /** @ngInject */
-    function planetService($log, $http) {
-        var uri = '/api/planets';
-        var service = {
-            resourceName: uri,
-            getPlanets: getPlanets
-        };
+  /** @ngInject */
+  function planetService($http) {
+    var uri = '/api/planets';
 
-        return service;
+    function service(){
+      //var self = this;
+      var planets = null;
 
-        function getPlanets(){
-            return $http.get(uri).then(getPlanetsComplete).catch(getPlanetsFailed);
+      service.prototype.fetchPlanets = function(){
+        return new Promise(function(resolve, reject){
+          return $http.get(uri).then(getPlanetsComplete).catch(getPlanetsFailed);
 
-            function getPlanetsComplete(response){
-                return response.data;
-            }
+          function getPlanetsComplete(response){
+            planets = response.data;
+            resolve(response.data);
+          }
 
-            function getPlanetsFailed(error){
-                $log.error('XHR Failed for getPlanets.\n' + angular.toJson(error.data, true));
-            }
+          function getPlanetsFailed(error){
+            reject('XHR Failed for getPlanets.\n' + angular.toJson(error.data, true));
+          }
+        });
+      };
+
+      service.prototype.getPlanets = function(){
+        if(planets != null){
+          return new Promise(function(resolve, reject){
+            resolve(planets);
+          });
         }
+        else{
+          return this.fetchPlanets();
+        }
+      };
     }
+
+    return new service();
+  }
 })();
