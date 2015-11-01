b905db39357a976d38af26713c16486b7b9517bb
wallart <julien.wallart@phelsuma.fr>
2015-11-01 01:39:18 +0100

    Setting up StellarSystem API

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,5 +1,6 @@
 import com.astrovisor.Description
 import com.astrovisor.Planet
+import com.astrovisor.Orbit
 import com.astrovisor.User
 import com.astrovisor.Trade
 import com.astrovisor.StellarSystem
@@ -7,6 +8,7 @@ import com.astrovisor.StellarSystem
 import grails.util.Environment
 
 import static com.astrovisor.Planet.Type.*
+import static com.astrovisor.Planet.Size.*
 
 class BootStrap {
 
@@ -48,19 +50,28 @@ class BootStrap {
     }
 
     def planetTestData() {
-        def system = StellarSystem.get(1)
+        def system = StellarSystem.get(2)
+        def orbit = new Orbit(semimajor_axis:10000, semiminor_axis:10000, orbital_speed:20.0, revolution_period:450);
 
         def planet = new Planet(code_name: "XO-000", age:0, name: 'kelto',
-            image: "mars", description: "desc", system:system,
-            type: GAS)
+            texture: "mars", description: "desc", system:system, orbit:orbit,
+            type: GAS, size: SMALL)
         assert planet.save(failOnError:true, flush:true, insert: true)
 
-        planet = new Planet(code_name: "XO-001",age:0, name: 'keltorin',
-            image: "pluto", description: "desc", system:system,
-            type: TELLURIC)
+        orbit = new Orbit(semimajor_axis:10000, semiminor_axis:10000, orbital_speed:250.0, revolution_period:450);
+        planet = new Planet(code_name: "XO-001", age:0, name: 'keltorin',
+            texture: "pluto", description: "desc", system:system, orbit:orbit,
+            type: TELLURIC, size: SMALL)
         assert planet.save(failOnError:true, flush:true, insert: true)
 
-        assert Planet.count == 2;
+        system = StellarSystem.get(1)
+        orbit = new Orbit(semimajor_axis:10000, semiminor_axis:10000, orbital_speed:50.0, revolution_period:365);
+        planet = new Planet(code_name: "XO-002", age:0, name: 'La Terre',
+                texture: "earth", description: "desc", system:system, orbit:orbit,
+                type: TELLURIC, size: SMALL, atmosphere: true)
+        assert planet.save(failOnError:true, flush:true, insert: true)
+
+        assert Planet.count == 3;
     }
 
     def descriptionTestData(){
--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -5,27 +5,25 @@ class UrlMappings {
         //all RestFull Controller
 
         group "/api", {
-//            "/systems/$id?"(controller: "stellarSystem") {
-//                action = [GET: "index"]
-//            }
+            "/systems/$id?"(controller: "stellarSystem") {
+                action = [GET: "index"]
+            }
 
-//            "/planets/$id?"(controller: "planet") {
-//                action = [GET: "index", PUT: "update", POST: "save", DELETE: "delete"]
-//            }
-            "/planets"(resources:"planet", includes:["index", "show"])
-//
-//
-//            "/descriptions/$id?"(controller: "description") {
-//                action = [GET: "index", PUT: "update", POST: "save", DELETE: "delete"]
-//            }
-//
-//            "/trades/$id?"(controller: "trade") {
-//                action = [GET: "index", PUT: "update", POST: "save", DELETE: "delete"]
-//            }
-//
-//            "/users/$id?"(controller: "user") {
-//                action = [POST: "save", PUT: "update"]
-//            }
+            "/planets/$id?"(controller: "planet") {
+                action = [GET: "index", PUT: "update", POST: "save", DELETE: "delete"]
+            }
+
+            "/descriptions/$id?"(controller: "description") {
+                action = [GET: "index", PUT: "update", POST: "save", DELETE: "delete"]
+            }
+
+            "/trades/$id?"(controller: "trade") {
+                action = [GET: "index", PUT: "update", POST: "save", DELETE: "delete"]
+            }
+
+            "/users/$id?"(controller: "user") {
+                action = [POST: "save", PUT: "update"]
+            }
         }
 
         "/"(view:"/index")
