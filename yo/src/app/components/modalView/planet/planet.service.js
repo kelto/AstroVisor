bf64d66c8f8668593b16b7539494d526a1d2e663
kelto <charles.fallourd@gmail.com>
2015-11-06 15:58:19 +0100

    create new service for planet and use it in planet controller

---
+++ yo/src/app/components/modalView/planet/planet.service.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+  angular.module('yo').service('planetService',planetService);
+
+  /* @ngInject */
+  function planetService($http) {
+    this.getDescriptionOfPlanet = function(planetId) {
+      return $http.get('/api/descriptions?planet=' + planetId);
+    };
+
+    this.getPlanet = function(planetId) {
+      return $http.get('/api/planets/' + planetId)
+    };
+
+    this.getTradesOfPlanet = function(planetId) {
+      $http.get('/api/trades?planet=' + planetId);
+    }
+
+  }
+})();
