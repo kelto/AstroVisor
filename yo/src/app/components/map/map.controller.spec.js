87f7f4fe0f102471835515685d5418d50cd12a11
wallart <julien.wallart@phelsuma.fr>
2015-11-07 19:41:43 +0100

    Updated map.controller test

--- yo/src/app/components/map/map.controller.spec.js
+++ yo/src/app/components/map/map.controller.spec.js
@@ -3,24 +3,27 @@
 
   describe('controller map', function(){
     var vm;
-    var $scope;
+    var $rootScope;
     var deferred;
 
     beforeEach(module('yo'));
-    beforeEach(inject(function(_$controller_, _$rootScope_, _systems_, _$q_) {
-      $scope = _$rootScope_;
+    beforeEach(inject(function(_$controller_, _$state_, _$rootScope_, _systems_, _$q_) {
+      $rootScope = _$rootScope_;
 
       deferred = _$q_.defer();
       spyOn(_systems_, 'getSystems').and.returnValue(deferred.promise);
 
       vm = _$controller_('MapController', {
+        $scope:$rootScope.$new(),
+        $rootScope:$rootScope,
+        state:_$state_,
         systems:_systems_
       });
     }));
 
     it('should define planets', function(){
       deferred.resolve([{name:'sys1', planets:[]}, {name:'sys2', planets:[]}, {name:'sys3', planets:[]}, {name:'sys4', planets:[]}]);
-      $scope.$apply();
+      $rootScope.$apply();
 
       expect(angular.isArray(vm.systems)).toBeTruthy();
       expect(vm.systems.length === 4).toBeTruthy();
