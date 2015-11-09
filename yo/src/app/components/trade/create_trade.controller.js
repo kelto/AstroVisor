40749f520fcc4fa51d911f494b397c3d81a90602
Edsolat <edouard.sabourdy@laposte.net>
2015-11-09 15:50:22 +0100

    Update create_trade_controller #47
    The trade is created in back end.

--- yo/src/app/components/trade/create_trade.controller.js
+++ yo/src/app/components/trade/create_trade.controller.js
@@ -1,4 +1,4 @@
-angular.module('yo').controller('CreateTradeController', function ($scope, $modalInstance, $auth,toastr) {
+angular.module('yo').controller('CreateTradeController', function ($scope, $modalInstance, $http, $stateParams, toastr) {
   /* jshint validthis: true */
   var vm = this;
   vm.trade = {
@@ -7,7 +7,13 @@ angular.module('yo').controller('CreateTradeController', function ($scope, $moda
   };
 
   vm.createTreade = function() {
-
+    $http.post('/api/trades/'+$stateParams.id, vm.trade).then(function() {
+      toastr.success('Trade "' + vm.trade.name +'" successfuly created!')
+      $modalInstance.close();
+    })
+      .catch(function() {
+        toastr.error('This username is probably used already.','Failed to signup');
+      });
   };
 
   vm.close = function() {
